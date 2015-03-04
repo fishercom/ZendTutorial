7539340d14f693131bd29a620f0960e73991eb0c
Fischer Tirado <fishdev@gmail.com>
2015-03-04 18:17:58 -0500

    avances controllers and views

--- module/Album/Module.php
+++ module/Album/Module.php
@@ -12,6 +12,11 @@ namespace Album;
 use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
 
+use Album\Model\Album;
+use Album\Model\AlbumTable;
+use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\TableGateway\TableGateway;
+
 class Module implements AutoloaderProviderInterface, ConfigProviderInterface{
 	public function getAutoloaderConfig()
 	{
@@ -27,8 +32,28 @@ class Module implements AutoloaderProviderInterface, ConfigProviderInterface{
 		);
 	}
 
+	public function getServiceConfig()
+	{
+		return array(
+			'factories' => array(
+				'Album\Model\AlbumTable' => function($sm) {
+					$tableGateway = $sm->get('AlbumTableGateway');
+					$table = new AlbumTable($tableGateway);
+					return $table;
+				},
+				'AlbumTableGateway' => function ($sm) {
+					$dbAdapter = $sm->get('Zend\Db\Adapter\Adapter');
+					$resultSetPrototype = new ResultSet();
+					$resultSetPrototype->setArrayObjectPrototype(new Album());
+					return new TableGateway('album', $dbAdapter, null, $resultSetPrototype);
+				},
+			),
+		);
+	}
+
 	public function getConfig()
 	{
 		return include __DIR__ . '/config/module.config.php';
 	}
+
 }
